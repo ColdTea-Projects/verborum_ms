7b8f399f958f3be613344bb6a198a29e2c59d92a
Yasar Naci Gunduz <yasarnaci.gunduz@nordsec.com>
2024-01-28 14:28:28 +0100

    initial commit

---
+++ ms_dictionary/src/main/java/de/coldtea/verborum/dictionary/MsDictionaryApplication.java
@@ -0,0 +1,13 @@
+package de.coldtea.verborum.dictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDictionaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsDictionaryApplication.class, args);
+    }
+
+}
---
+++ ms_dictionary/src/test/java/de/coldtea/verborum/dictionary/MsDictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package de.coldtea.verborum.dictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDictionaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
