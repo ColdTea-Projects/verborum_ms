c47d32c133b9cbb5b4de52d0351ec23278fd3a3b
Yasar Naci Gunduz <yasarnaci.gunduz@nordsec.com>
2024-02-07 21:39:15 +0100

    add serial id for exception

--- ms_dictionary/src/main/java/de/coldtea/verborum/msdictionary/common/exception/InvalidUUIDException.java
+++ ms_dictionary/src/main/java/de/coldtea/verborum/msdictionary/common/exception/InvalidUUIDException.java
@@ -1,6 +1,11 @@
 package de.coldtea.verborum.msdictionary.common.exception;
 
+import java.io.Serial;
+
 public class InvalidUUIDException extends IllegalArgumentException{
+    @Serial
+    private static final long serialVersionUID = -5365123123856068000L;
+
     public InvalidUUIDException(String fieldName) {
         super(fieldName);
     }
